05bb9f99dcab1d1e635af4fc3b6951fc131716d8
hornauerp <philipp.hornauer@bsse.ethz.ch>
2022-02-10 18:07:58 +0100

    added example sorting files

---
+++ Data/Sorting_example/params.py
@@ -0,0 +1,7 @@
+dat_path = giverandomname.dat
+n_channels_dat = 1006
+n_features_per_channel = 5
+dtype = r"float32"
+sample_rate = 20000.0
+dir_path = r"/cluster/project/bsse_sdsc/BELUB/Measurements/Cell_line_9/200218/5206/Sorting/recording/recording.GUI"
+hp_filtered = True
